aacaa787f28f46bcdd29cd975319949c2082d3ad
Jerry Brown <jbbrown@wpi.edu>
2018-12-21 21:05:12 -0500

    Day 2 part 2

--- 2018/two/solution.py
+++ 2018/two/solution.py
@@ -1,3 +1,12 @@
+def common_letters(a, b):
+    list_result = []
+
+    for i in range(len(a)):
+        if (a[i] == b[i]):
+            list_result.append(a[i])
+    
+    return list_result
+
 with open("two/input.txt", "r") as box_ids_file:
     box_ids = [box_id.strip() for box_id in box_ids_file.readlines()]
 
@@ -25,4 +34,15 @@ with open("two/input.txt", "r") as box_ids_file:
             num_three_repeating += 1
     
     checksum = num_two_repeating * num_three_repeating
-    print(checksum)
+    print("Checksum: {}".format(checksum))
+
+    found_common = False
+    for i, box_id_1 in enumerate(box_ids):
+        for box_id_2 in box_ids[i:]:
+            common = common_letters(box_id_1, box_id_2)
+            if (len(common) == len(box_id_1) - 1):
+                print("Common letters: {}".format(''.join(common)))
+                found_common = True
+                break
+        if (found_common):
+            break
