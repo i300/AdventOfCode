b66c79e5ba841c3ba2181cf8dbe956ac1fc54026
Jerry Brown <jbbrown@wpi.edu>
2018-12-21 20:33:10 -0500

    Part 2 for day 1

--- 2018/one/solution.py
+++ 2018/one/solution.py
@@ -1,9 +1,19 @@
 with open("one/input.txt", "r") as freqs_file:
     freqs = [freq.strip() for freq in freqs_file.readlines()]
+    foundFreqs = {}
 
     totalFreq = 0
-    for freq in freqs:
-        if (freq != ''):
-            totalFreq += int(freq.strip())
-
-    print(totalFreq)
+    loopFreq = 0
+    foundReapeatedFreq = False
+    while not foundReapeatedFreq:
+        loopFreq = 0
+        for freq in freqs:
+            if (freq != ''):
+                totalFreq += int(freq)
+                loopFreq += int(freq)
+                if (totalFreq in foundFreqs and not foundReapeatedFreq):
+                    print("Repeated Freq: {}".format(totalFreq))
+                    foundReapeatedFreq = True
+                foundFreqs[totalFreq] = "Y"
+    
+    print("Device Output: {}".format(loopFreq))
