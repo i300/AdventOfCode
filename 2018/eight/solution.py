9d354b30b43764db53ea9210d4a0c3d306bcd75b
Jerry Brown <jbbrown@wpi.edu>
2018-12-23 12:45:41 -0500

    Day 8 part 2

--- 2018/eight/solution.py
+++ 2018/eight/solution.py
@@ -20,18 +20,40 @@ class Data():
         self.index += 1
         return self.index
 
-def parse_node(data):
+def calc_metadata_sum(data):
     num_subnodes = data.get_next_data()
     num_metadata = data.get_next_data()
 
     metadata_sum = 0
     for _ in range(num_subnodes):
-        metadata_sum += parse_node(data)
+        metadata_sum += calc_metadata_sum(data)
 
     for _ in range(num_metadata):
         metadata_sum += data.get_next_data()
 
     return metadata_sum
+
+def calc_node_value(data):
+    num_subnodes = data.get_next_data()
+    num_metadata = data.get_next_data()
+    node_value = 0
+
+    if (num_subnodes == 0):
+        for _ in range(num_metadata):
+            node_value += data.get_next_data()
+    else:
+        subnode_values = []
+        for _ in range(num_subnodes):
+            subnode_values.append(calc_node_value(data))
+
+        for _ in range(num_metadata):
+            metadata_value = data.get_next_data()
+            try:
+                node_value += subnode_values[metadata_value - 1]
+            except IndexError:
+                pass
+    
+    return node_value
 
 if __name__ == "__main__":
     data = []
@@ -39,6 +61,9 @@ if __name__ == "__main__":
         data = [int(x) for x in input_file.readline().strip().split(" ")]
 
     data_class = Data(data)
-    sum_metadata = parse_node(data_class)
+    sum_metadata = calc_metadata_sum(data_class)
     print("Metadata Sum: {}".format(sum_metadata))
-    
+
+    data_class = Data(data)
+    root_value = calc_node_value(data_class)
+    print("Root Node Value: {}".format(root_value))
